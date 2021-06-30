f81d4ee51b4cef09c1413dcefc9b03c58358ab88
c0venn <fabichidori2@gmail.com>
2021-06-30 21:07:14 +0000

    finished

--- src/app.py
+++ src/app.py
@@ -1,17 +1,23 @@
-from flask import Flask, jsonify, request 
+from flask import Flask, jsonify, request
+import json
 app = Flask(__name__)
-todos = { "label": "My first task", "done": False }
+todos = [{ "label": "My first task", "done": False }]
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
-    json_text = jsonify(todos)
-    return json_text
+    return jsonify(todos)
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.data
-    print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
+    decode_object = json.loads(request_body)
+    todos.append(decode_object)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    return jsonify(todos)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
